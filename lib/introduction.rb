db9521cf1fa59b5b48ef29b02b4283211ff853a7
Riley Iverson <rmiverson99@gmail.com>
2020-10-15 21:00:27 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,11 +7,11 @@ def introduction name
   return "Hi, my name is #{name}."
 end
 
-def introduction_with_language (name, language)
+def introduction_with_language(name, language)
   return "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language_optional (name, language = "Ruby")
+def introduction_with_language_optional(name, language = "Ruby")
   return "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
